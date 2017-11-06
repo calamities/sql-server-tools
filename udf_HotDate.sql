ac2bd6917cbe1f9dd7cb4d0bdd5811680e992f5b
calamities <calamities2@gmail.com>
2017-11-05 23:33:17 -0500

    added day of week, month, and ismonth indicator

--- udf_HotDate.sql
+++ udf_HotDate.sql
@@ -8,10 +8,13 @@
 -- start and end date.
 -- =============================================
 
-CREATE FUNCTION dbo.udf_HotDate (@datestart smalldatetime, @dateend smalldatetime)
+CREATE FUNCTION dbo.udf_HotDate (@datestart datetime2, @dateend datetime2)
 RETURNS @calendar TABLE (
   [day] int,
-  [date] smalldatetime
+  [date] datetime2,
+  [weekday] varchar(50),
+  [month] varchar(50),
+  [ismonth] varchar(50)
 )
 AS
 
@@ -33,9 +36,9 @@ BEGIN
 
   END
 
-  UPDATE a
-  SET [date] = DATEADD(DAY, [day] - 1, @datestart)
-  --select *, DATEADD(day,id-1,@datestart)
+ UPDATE a
+  SET [date] = DATEADD(DAY, [day] - 1, @datestart), weekday=datepart(dw,DATEADD(day,day-1,@datestart)), month = datepart(mm,DATEADD(day,day-1,@datestart)), ismonth = case when datepart(d,DATEADD(day,day-1,@datestart)) =1 then 1 else 0 end
+  --select *, DATEADD(day,day-1,@datestart), datepart(dw,DATEADD(day,day-1,@datestart)), case when datepart(d,DATEADD(day,day-1,@datestart)) =1 then datepart(mm,DATEADD(day,day-1,@datestart)) else 0 end
   FROM @calendar a
 
   RETURN
