b83ab5f1ed2fdf46fda2127379f0a97a8c9fbf3a
calamities <calamities2@gmail.com>
2017-11-05 23:37:07 -0500

    added isweekday indicator and renamed weekday to dayofweek. also changed indicators to bit type.

--- udf_HotDate.sql
+++ udf_HotDate.sql
@@ -8,13 +8,14 @@
 -- start and end date.
 -- =============================================
 
-CREATE FUNCTION dbo.udf_HotDate (@datestart datetime2, @dateend datetime2)
+alter FUNCTION dbo.udf_HotDate (@datestart datetime2, @dateend datetime2)
 RETURNS @calendar TABLE (
   [day] int,
   [date] datetime2,
-  [weekday] varchar(50),
+  [dayofweek] int,
+  [isweekday] bit,
   [month] varchar(50),
-  [ismonth] varchar(50)
+  [ismonth] bit
 )
 AS
 
@@ -37,9 +38,10 @@ BEGIN
   END
 
  UPDATE a
-  SET [date] = DATEADD(DAY, [day] - 1, @datestart), weekday=datepart(dw,DATEADD(day,day-1,@datestart)), month = datepart(mm,DATEADD(day,day-1,@datestart)), ismonth = case when datepart(d,DATEADD(day,day-1,@datestart)) =1 then 1 else 0 end
+  SET [date] = DATEADD(DAY, [day] - 1, @datestart), dayofweek=datepart(dw,DATEADD(day,day-1,@datestart)),isweekday = case when datepart(dw,DATEADD(day,day-1,@datestart)) not in(7,1) then 1 else 0 end, month = datepart(mm,DATEADD(day,day-1,@datestart)), ismonth = case when datepart(d,DATEADD(day,day-1,@datestart)) =1 then 1 else 0 end
   --select *, DATEADD(day,day-1,@datestart), datepart(dw,DATEADD(day,day-1,@datestart)), case when datepart(d,DATEADD(day,day-1,@datestart)) =1 then datepart(mm,DATEADD(day,day-1,@datestart)) else 0 end
   FROM @calendar a
+
 
   RETURN
 END
